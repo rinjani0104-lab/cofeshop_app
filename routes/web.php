f4b689a2e5c7a2f608e1e829232802f97c4a1b0c
Stella <febrielmeziadinaldi@gmail.com>
2026-01-20 09:39:38 +0700

    routing sementara

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/menu', function () {
+    return view('menu.index');
+});
+
+Route::get('/menu/detail', function () {
+    return view('menu.show');
+});
+
+Route::get('/order', function () {
+    return view('order.form');
+});
+
+Route::get('/reservation', function () {
+    return view('reservation');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
