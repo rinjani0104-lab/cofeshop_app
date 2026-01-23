078da43c3ad0248990b3e32acb0fd7e74f429b8b
Stella <febrielmeziadinaldi@gmail.com>
2026-01-23 17:03:10 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route::get('/reservation', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
